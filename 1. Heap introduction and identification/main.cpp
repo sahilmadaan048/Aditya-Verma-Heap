ad97241dce8b7bee371a679e32be1ce4f2e483bc
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-07 22:54:32 +0530

    how to identigy heaps

---
+++ 1. Heap introduction and identification/main.cpp
@@ -0,0 +1,24 @@
+// https://www.youtube.com/watch?v=hW8PrQrvMNc&list=PL_z_8CaSLPWdtY9W22VjnPxG30CXNZpI9
+
+/*
+1)A Heap is a special Tree-based data structure in which the tree is a complete binary tree. Generally, Heaps can be of two types:
+
+Max-Heap: In a Max-Heap the key present at the root node must be greatest among the keys present at all of it’s children. The same property must be recursively true for all sub-trees in that Binary Tree.
+
+Min-Heap: In a Min-Heap the key present at the root node must be minimum among the keys present at all of it’s children. The same property must be recursively true for all sub-trees in that Binary Tree. . 
+
+*/
+
+/*
+
+1. k
+
+2. smallest/ largest
+
+k + smallest => maxheap => maximm elements upar
+k + largest => minheap => minimum elements upar
+
+
+all heap quesstiona are heap ques inly => n log n TC becomes n log k
+
+*/
