054b89d8f3c8726c59451e6628c46403a70a2e1b
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-08 18:31:55 +0530

    connect ropes to minimise the cost

---
+++ 9. Connect ropes to minimise the cost/main.cpp
@@ -0,0 +1,100 @@
+// https://www.youtube.com/watch?v=_k_c9nqzKN0&list=PL_z_8CaSLPWdtY9W22VjnPxG30CXNZpI9&index=9
+
+/*
+
+There are given n ropes of different lengths, we need to connect these ropes into one rope. The cost to connect two ropes is equal to sum of their lengths. We need to connect the ropes with minimum cost.
+
+For example if we are given 4 ropes of lengths 4, 3, 2 and 6. We can connect the ropes in following ways.
+1) First connect ropes of lengths 2 and 3. Now we have three ropes of lengths 4, 6 and 5.
+2) Now connect ropes of lengths 4 and 5. Now we have two ropes of lengths 6 and 9.
+3) Finally connect the two ropes and all ropes have connected.
+
+Total cost for connecting all ropes is 5 + 9 + 15 = 29. This is the optimized cost for connecting ropes. Other ways of connecting ropes would always have same or more cost. For example, if we connect 4 and 6 first (we get three strings of 3, 2 and 10), then connect 10 and 3 (we get two strings of 13 and 2). Finally we connect 13 and 2. Total cost in this way is 10 + 13 + 15 = 38. . 
+
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve() {
+    // Example input
+    vector<int> ropes = {4, 3, 2, 6};
+    
+    // Min-heap to store the ropes' lengths
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+
+    // Insert all ropes into the heap
+    for (int rope : ropes) {
+        min_heap.push(rope);
+    }
+
+    int total_cost = 0;
+
+    // Continue until there's only one rope left
+    while (min_heap.size() > 1) {
+        // Extract the two shortest ropes
+        int rope1 = min_heap.top();
+        min_heap.pop();
+        int rope2 = min_heap.top();
+        min_heap.pop();
+
+        // The cost to connect the two ropes
+        int cost = rope1 + rope2;
+        total_cost += cost;
+
+        // Push the new connected rope back into the heap
+        min_heap.push(cost);
+    }
+
+    // Output the total cost to connect all ropes
+    cout << "Minimum cost to connect all ropes: " << total_cost << endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
