c8eb6f5e99eba77f4296200cbc923ae1ffedcd15
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-08 16:53:30 +0530

    k sorted array done

---
+++ 4. Sort a k sorted array/main.cpp
@@ -0,0 +1,95 @@
+// https://www.youtube.com/watch?v=dYfM6J1y0mU&list=PL_z_8CaSLPWdtY9W22VjnPxG30CXNZpI9&index=4
+
+/*
+
+Given an array of n elements, where each element is at most k away from its target position, devise an algorithm that sorts in O(n log k) time. For example, let us consider k is 2, an element at index 7 in the sorted array, can be at indexes 5, 6, 7, 8, 9 in the given array.
+
+Example:
+Input : arr[] = {6, 5, 3, 2, 8, 10, 9}
+k = 3 
+Output : arr[] = {2, 3, 5, 6, 8, 9, 10} . 
+ 
+
+
+4 Sort a K Sorted Array | Sort Nearly Sorted Array 
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve(){
+    vector<int> arr = {6, 5, 3, 2, 8, 10, 9};
+    int k = 3;
+    int n = arr.size();
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++) {
+        minHeap.push(arr[i]);
+
+        // If heap size exceeds k, pop the top and add to result
+        if (minHeap.size() > k) {
+            ans.push_back(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    // Pop remaining elements
+    while (!minHeap.empty()) {
+        ans.push_back(minHeap.top());
+        minHeap.pop();
+    }
+
+    cout << ans << "\n";  // output the sorted array
+}
+
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
